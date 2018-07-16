96406ab8d39b19836254efd08fca7470fa82b9e0
osss <bilibilis@163.com>
2018-07-16 13:42:22 +0800

    Apply Damage to Actors

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SWeapon.h"
+#include "DrawDebugHelpers.h"
+#include "Kismet/GameplayStatics.h"
 
 
 // Sets default values
@@ -18,6 +20,35 @@ ASWeapon::ASWeapon()
 void ASWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+	
+}
+
+void ASWeapon::Fire()
+{
+	AActor* MyOwner = GetOwner();
+	if (MyOwner) {
+		FVector EyeLocation;
+		FRotator EyeRotation;
+
+		MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+		FVector ShotDircetion = EyeRotation.Vector();
+		FVector TraceEnd = EyeLocation + (ShotDircetion * 10000);
+
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(MyOwner);
+		QueryParams.AddIgnoredActor(this);
+		QueryParams.bTraceComplex = true;
+
+		FHitResult Hit;
+		if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, ECC_Visibility, QueryParams)) {
+			//Block Hit!
+			AActor* HitActor = Hit.GetActor();
+			UGameplayStatics::ApplyPointDamage(HitActor, 20.f, ShotDircetion, Hit, MyOwner->GetInstigatorController(), this, DamageType);
+		}
+		DrawDebugLine(GetWorld(),EyeLocation,TraceEnd,FColor::Red,false,1.f,0,1.f);
+	}
+
 	
 }
 
